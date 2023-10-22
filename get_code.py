c54a982990c5687ae809c95aad3256ea9fc05492
origami <gren.aula1214@gmail.com>
2023-10-23 08:59:56 +0900

    first commit

---
+++ get_code.py
@@ -0,0 +1,126 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+import re
+
+#code
+mail_code = 0
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+import base64, email #デコード用
+import dateutil.parser
+
+#token.jsonを設定
+tokenPath = "token.json"
+
+#credentials.jsonを設定
+credentialsPath = "credentials.json"
+
+
+#メール本文のデコード
+def decode(encoded):
+   decoded = base64.urlsafe_b64decode(encoded).decode()
+   return decoded
+
+
+#初期化(from quickstart.py)
+def gmail_init():
+   creds = None
+   # The file token.json stores the user's access and refresh tokens, and is
+   # created automatically when the authorization flow completes for the first
+   # time.
+   if os.path.exists(tokenPath):
+       creds = Credentials.from_authorized_user_file(tokenPath, SCOPES)
+   # If there are no (valid) credentials available, let the user log in.
+   if not creds or not creds.valid:
+       if creds and creds.expired and creds.refresh_token:
+           creds.refresh(Request())
+       else:
+           flow = InstalledAppFlow.from_client_secrets_file(
+               credentialsPath, SCOPES)
+           creds = flow.run_local_server(port=0)
+       # Save the credentials for the next run
+       with open(tokenPath, 'w') as token:
+           token.write(creds.to_json())
+
+   service = build('gmail', 'v1', credentials=creds)
+   return service
+
+#メール本文の内容を配列で取得する関数
+def gmail_get_messages_body(service, labelIdsValue):
+   mailBody = []
+
+   # メッセージの一覧を取得
+   messages = service.users().messages()
+   msg_list = messages.list(userId='me', labelIds = labelIdsValue, maxResults = 10).execute()
+   # msg_list = messages.list(userId='me', labelIds=labelIdsValue ,maxResults=456).execute() #最大値指定
+
+   # 取得したメッセージの一覧を配列に格納
+   for msg in msg_list['messages']:
+
+       #メールの受信日時を取得
+       date = gmail_get_messages_body_date(messages,msg)
+
+       topid = msg['id']
+       msg = messages.get(userId='me', id=topid).execute()
+
+       if(msg["payload"]["body"]["size"]!=0):
+           mailBody.append(date+"<br>"+decode(msg["payload"]["body"]["data"]))
+       else:
+           #メールによっては"parts"属性の中に本文がある場合もある
+           mailBody.append(date+"<br>"+decode(msg["payload"]["parts"][0]["body"]["data"]))
+
+   return mailBody
+
+
+#gmail_get_messages_body関数内で受信日時を取得する関数
+def gmail_get_messages_body_date(messages,msg):
+   msg_id = msg['id']
+   m = messages.get(userId='me', id=msg_id, format='raw').execute()
+   raw = base64.urlsafe_b64decode(m['raw'])
+
+   # Emailを解析する
+   eml = email.message_from_bytes(raw)
+
+   date = dateutil.parser.parse(eml.get('Date')).strftime("%Y-%m-%d_%H-%M-%S")
+   return date
+
+#ラベルのIDやnameを表示する関数
+def gmail_display_label(service):
+   results = service.users().labels().list(userId='me').execute()
+   labels = results.get('labels', [])
+
+   if not labels:
+       print('No labels found.')
+   else:
+       print('Labels:')
+       for label in labels:
+           print(label)
+
+def authentication_code():
+
+    #ここからやりたい処理を書く
+    service = gmail_init()
+
+    #ラベル情報を取得
+    #gmail_display_label(service)
+
+    #ラベル[code]内のメール取得
+    bodyArray = gmail_get_messages_body(service, "ラベルを入力")
+
+    bodyList = re.split('<br>|\r\n', bodyArray[0])
+
+    #メッセージ全体の表示
+    '''
+    for body in bodyList:
+        print(body)
+    '''
+    global mail_code
+    mail_code = bodyList[3]
+    #認証コード部分の取得
---
+++ login.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+import time
+import chromedriver_binary
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome import service
+from selenium.webdriver.chrome.options import Options
+
+import get_code
+
+Student_ID = "Your Student ID"
+Password = "Your Password"
+
+
+def auto_login():
+    #ログイン後ブラウザが自動で閉じないように起動時にdetachオプションをTrueにする
+    #ソフトウェアによって制御されていますを非表示
+    #パスワードを保存するを非表示
+    options = Options()
+    options.add_experimental_option('detach', True)
+    options.add_experimental_option("excludeSwitches", ['enable-automation', 'load-extension'])
+    prefs = {'profile.password_manager_enabled' : False,
+		     'credentials_enable_service': False}
+    options.add_experimental_option("prefs", prefs)
+
+    #LMSへの画面遷移
+    driver = webdriver.Chrome(options=options)
+    driver.get('URL')
+
+    login_button = driver.find_element(By.ID, "loginbutton")
+    login_button.click()
+
+    #待機
+    time.sleep(2)
+
+    #入力場所の情報取得
+    username = driver.find_element(By.NAME, "username")
+    password = driver.find_element(By.NAME, "password")
+    login = driver.find_element(By.ID, "login_button")
+
+    #ユーザ名入力
+    username.clear()
+    username.send_keys(Student_ID)
+
+    #パスワード入力
+    password.clear()
+    password.send_keys(Password)
+
+    #ログインボタン
+    login.click()
+
+    #ページ遷移待機(URLの変更待ち)
+    time.sleep(5)
+
+    #認証コード取得をlogin_code.pyを利用
+    authentication_code = driver.find_element(By.NAME, "password")
+    get_code.authentication_code()
+    code = get_code.mail_code
+
+    #コード確認用
+    #print(code)
+
+    #認証コード入力
+    authentication_code.clear()
+    authentication_code.send_keys(code)
+
+    #ページが遷移したので再度ログインボタンの情報取得
+    input_login = driver.find_element(By.ID, "login_button")
+    input_login.click()
+
+auto_login()
